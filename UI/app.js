639f92a2633242ecbb9e3f8465c8a3101bdc6c63
sarthak2304 <sarthaksanwal029@gmail.com>
2023-07-01 22:07:25 +0530

    first release

---
+++ UI/app.js
@@ -0,0 +1,76 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = " http://127.0.0.1:5000/classify_image";
+        
+
+        $.post(url, {
+            image_data: imageData
+        },function(data, status) {
+            
+            console.log(data);
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            // let players = ["anderson_silva", "conor_mcgregor", "dustin_poirier", "george_stpiere", "jon_jones", "khabib_nurmagomedov"];
+        
+            let match = null;
+            let bestScore = -1;
+            for (let i=0;i<data.length;++i) {
+                let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                if(maxScoreForThisClass>bestScore) {
+                    match = data[i];
+                    bestScore = maxScoreForThisClass;
+                }
+            }
+            if (match) {
+                $("#error").hide();
+                $("#resultHolder").show();
+                $("#resultHolder").html($(`[data-player="${match.class.replace(/ /g, "_")}"`).html());
+                $("#divClassTable").show();
+                let classDictionary = match.class_dictionary;
+                for(let personName in classDictionary) {
+                    let index = classDictionary[personName];
+                    let proabilityScore = match.class_probability[index];
+                    let elementName = "#score_" + personName.replace(/ /g, "_");
+                    console.log(personName, proabilityScore, elementName);
+                    $(elementName).html(proabilityScore);
+                }
+            }
+            // dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
